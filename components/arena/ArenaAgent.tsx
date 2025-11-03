51c2045d425e3126a71d3c8e03562cc3c0bf840d
vchat-meme-blip <limeikenji@gmail.com>
2025-11-03 12:02:59 +0200

    Model Room positioning

--- components/arena/ArenaAgent.tsx
+++ components/arena/ArenaAgent.tsx
@@ -41,7 +41,8 @@ export default function ArenaAgent({ agent, isSpeaking }: ArenaAgentProps) {
             idleUrl={idleAnimation}
             triggerAnimationUrl="/animations/gesture_talk.vrma"
             triggerKey={animationTriggerKey}
-            disableAutoGrounding={false}
+            disableAutoGrounding={true}
+            verticalOffset={-1.0}
         />
     );
 };
